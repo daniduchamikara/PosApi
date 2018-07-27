fd0784fae9cf2156a7b58f2175088bfa761496e3
daniduchamikara@gmail.com <udani1009>
2018-07-27 10:30:46 +0530

    commit file to be initialize

---
+++ src/main/java/PosApiApp.java
@@ -0,0 +1,13 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableAutoConfiguration
+public class PosApiApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PosApiApp.class,args);
+    }
+}
